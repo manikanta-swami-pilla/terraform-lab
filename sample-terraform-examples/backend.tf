cdc1b00580d8783ca221f531516b93df1bec6fff
CreativeRepos <56748187+CreativeRepos@users.noreply.github.com>
2026-02-14 15:36:50 +0530

    backend file

--- sample-terraform-examples/backend.tf
+++ sample-terraform-examples/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     bucket         = "tf-state-bucket-2026-02-12" # change this
-    key            = "mani/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "ap-south-2"
     encrypt        = true
-    dynamodb_table = "terraform-lock"
+    use_lockfile  = true
   }
 }
