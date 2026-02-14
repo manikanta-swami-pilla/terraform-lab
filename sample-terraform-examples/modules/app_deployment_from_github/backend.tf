0e10f675c347aa89bc18674f616ed4fc92ed9e3c
CreativeRepos <56748187+CreativeRepos@users.noreply.github.com>
2026-02-14 21:03:43 +0530

    removed app.py

--- sample-terraform-examples/modules/app_deployment_from_github/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "tf-state-and-lock-bucket-2026-02-12" # change this
-    key          = "terraform.tfstate"
-    region       = "ap-south-2"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
