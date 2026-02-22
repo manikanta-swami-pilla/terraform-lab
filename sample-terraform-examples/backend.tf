cbdd0c9ffed7505683d9ccdd0df2af08ede58041
CreativeRepos <56748187+CreativeRepos@users.noreply.github.com>
2026-02-22 13:07:07 +0530

    backend

--- sample-terraform-examples/backend.tf
+++ sample-terraform-examples/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "tf-state-and-lock-bucket-2026-02-12"
-    key          = "state/terraform.tfstate"
+    key          = "terraform.tfstate"
     region       = "ap-south-2"
     encrypt      = true
     use_lockfile = true
